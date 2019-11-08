2f74c0f4342b6751a849be88f79d978250ebeb9e
zephyrmay <1017059987@qq.com>
2019-11-08 15:59:41 +0800

    first commit

---
+++ src/main/java/com/anxing/anxingservice/AnxingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.anxing.anxingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnxingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnxingServiceApplication.class, args);
+    }
+
+}
